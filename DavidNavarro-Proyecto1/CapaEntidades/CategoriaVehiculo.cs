c42edef2b39927df07a30101315a0887d3f0e21b
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-17 11:00:06 -0600

    clases CategoriaVehiculo y Vehiculo creadas

---
+++ DavidNavarro-Proyecto1/CapaEntidades/CategoriaVehiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class CategoriaVehiculo
+    {
+        public int IdCategoria {  get; set; }
+        public string NombreCategoria { get; set; }
+        public string Descripcion { get; set; }
+
+        public CategoriaVehiculo(int idCategoria, string nombreCategoria, string descripcion)
+        {
+            IdCategoria = idCategoria;
+            NombreCategoria = nombreCategoria;
+            Descripcion = descripcion;
+        }
+    }
+}
---
+++ DavidNavarro-Proyecto1/CapaEntidades/Vehiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Vehiculo
+    {
+        public int IdVehiculo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; } //Se utiliza "Anio" en vez de "Año" para evitar problemas de compatibilidad
+        public decimal Precio { get; set; }
+        public CategoriaVehiculo Categoria { get; set; }
+        public char Estado { get; set; }
+
+    }
+}
