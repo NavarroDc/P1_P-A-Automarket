84d07bb03607222d4679265680cfa29760202163
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-17 11:21:25 -0600

    se crea la clase sucursal

---
+++ DavidNavarro-Proyecto1/CapaEntidades/Sucursal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Sucursal
+    {
+        public int IdSucursal { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public Vendedor VendedorEncargado { get; set; }
+        public bool Activo { get; set; }
+
+        public Sucursal(int idSucursal, string nombre, string direccion, string telefono, Vendedor vendedorEncargado, bool activo)
+        {
+            IdSucursal = idSucursal;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            VendedorEncargado = vendedorEncargado;
+            Activo = activo;
+        }
+    }
+}
