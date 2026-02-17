8befd79d4684189ee264ad1c2a13e841095409ca
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-17 11:39:27 -0600

    se crea la clase Cliente y la clase VehiculoXSucursal

---
+++ DavidNavarro-Proyecto1/CapaEntidades/VehiculoXSucursal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class VehiculoXSucursal
+    {
+        public Sucursal Sucursal { get; set; }
+        public Vehiculo Vehiculo { get; set; }
+        public int Cantidad { get; set; }
+
+        public VehiculoXSucursal(Sucursal sucursal, Vehiculo vehiculo, int cantidad)
+        {
+            Sucursal = sucursal;
+            Vehiculo = vehiculo;
+            Cantidad = cantidad;
+        }
+    }
+}
