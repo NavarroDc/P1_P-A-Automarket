19161c1a2ec81c90a06e1d6d32aa1c3c64d4dde2
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-20 00:35:38 -0600

    commit mientras

---
+++ DavidNavarro-Proyecto1/CapaLogica/CategoriaVehiculoLogica.cs
@@ -0,0 +1,48 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class CategoriaVehiculoLogica
+    {
+        private CategoriaVehiculoDatos datosCategoria;
+
+        public CategoriaVehiculoLogica()
+        {
+            datosCategoria = new CategoriaVehiculoDatos();
+        }
+
+        public bool agregarCategoria(int id, string nombre, string descripcion)
+        {
+            //Validar que el id sea único 
+            if (datosCategoria.BuscarCatId(id) != null)
+            {
+                return false;
+            }
+
+            //Validar campos vacíos en categoría
+            if (string.IsNullOrEmpty(nombre))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(descripcion))
+            {
+                return false;
+            }
+
+            //Crea el objeto
+            CategoriaVehiculo nuevaCategoria = new CategoriaVehiculo(id, nombre, descripcion);
+
+            //Guarda el objeto en CapaDatos
+            return datosCategoria.agregarCategoria(nuevaCategoria);
+        }
+
+        
+    }
+}
