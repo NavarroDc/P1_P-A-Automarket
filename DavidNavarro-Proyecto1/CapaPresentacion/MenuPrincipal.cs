f4800b6f61160b35442005b1e872a8ce7b4cbded
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-21 13:28:37 -0600

    ajustes label

--- DavidNavarro-Proyecto1/CapaPresentacion/MenuPrincipal.cs
+++ DavidNavarro-Proyecto1/CapaPresentacion/MenuPrincipal.cs
@@ -22,6 +22,16 @@ namespace CapaPresentacion
 
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void btnRegistrarCategoria_Click(object sender, EventArgs e)
         {
 
