fe6ccac658b15ae9ae493fcdabb110d1114d60df
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-21 14:05:40 -0600

    diseño registrar categoría

--- DavidNavarro-Proyecto1/CapaPresentacion/formRegistrarCategoria.Designer.cs
+++ DavidNavarro-Proyecto1/CapaPresentacion/formRegistrarCategoria.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             btnAtras = new Button();
+            label1 = new Label();
+            txtIdCategoria = new TextBox();
+            label2 = new Label();
+            txtNombreCategoria = new TextBox();
+            label3 = new Label();
+            txtDescripcionCat = new TextBox();
+            btnGuardar = new Button();
             SuspendLayout();
             // 
             // btnAtras
@@ -41,19 +48,91 @@
             btnAtras.UseVisualStyleBackColor = true;
             btnAtras.Click += btnAtras_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(151, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Ingrese el ID de la categoría";
+            // 
+            // txtIdCategoria
+            // 
+            txtIdCategoria.Location = new Point(193, 26);
+            txtIdCategoria.Name = "txtIdCategoria";
+            txtIdCategoria.Size = new Size(136, 23);
+            txtIdCategoria.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(182, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Ingrese el nombre de la categoría";
+            // 
+            // txtNombreCategoria
+            // 
+            txtNombreCategoria.Location = new Point(220, 96);
+            txtNombreCategoria.Name = "txtNombreCategoria";
+            txtNombreCategoria.Size = new Size(136, 23);
+            txtNombreCategoria.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 168);
+            label3.Name = "label3";
+            label3.Size = new Size(201, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Ingrese la descripción de la categoría";
+            // 
+            // txtDescripcionCat
+            // 
+            txtDescripcionCat.Location = new Point(239, 168);
+            txtDescripcionCat.Name = "txtDescripcionCat";
+            txtDescripcionCat.Size = new Size(135, 23);
+            txtDescripcionCat.TabIndex = 6;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(28, 242);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.TabIndex = 7;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
             // formRegistrarCategoria
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnGuardar);
+            Controls.Add(txtDescripcionCat);
+            Controls.Add(label3);
+            Controls.Add(txtNombreCategoria);
+            Controls.Add(label2);
+            Controls.Add(txtIdCategoria);
+            Controls.Add(label1);
             Controls.Add(btnAtras);
             Name = "formRegistrarCategoria";
             Text = "formRegistrarCategoria";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnAtras;
+        private Label label1;
+        private TextBox txtIdCategoria;
+        private Label label2;
+        private TextBox txtNombreCategoria;
+        private Label label3;
+        private TextBox txtDescripcionCat;
+        private Button btnGuardar;
     }
 }
--- DavidNavarro-Proyecto1/CapaPresentacion/formRegistrarCategoria.cs
+++ DavidNavarro-Proyecto1/CapaPresentacion/formRegistrarCategoria.cs
@@ -21,5 +21,10 @@ namespace CapaPresentacion
         {
             this.Close();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
