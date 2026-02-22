58803861893cfb6129e82d8c0364668f849d616f
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2026-02-22 10:27:16 -0600

    métodos en VehiculoDatos

--- DavidNavarro-Proyecto1/CapaDatos/VehiculoDatos.cs
+++ DavidNavarro-Proyecto1/CapaDatos/VehiculoDatos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaEntidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,55 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    internal class VehiculoDatos
+    public class VehiculoDatos
     {
+        private Vehiculo[] vehiculos;
+        private int contador;
+
+        //Constructor
+        public VehiculoDatos()
+        {
+            vehiculos = new Vehiculo[50];
+            contador = 0;
+        }
+
+        //Método para agregar un vehículo al arreglo
+        public bool agregarVehiculo(Vehiculo nuevoVehiculo)
+        {
+            if(contador >= vehiculos.Length)
+            {
+                return false;//Falso porque el arreglo está lleno
+            }
+
+            vehiculos[contador] = nuevoVehiculo;
+            contador++;
+            return true;
+        }
+
+        //Buscar vehículo por el ID
+        public Vehiculo buscarVehiculoId(int id)
+        {
+            for(int i = 0; i < contador;  i++)
+            {
+                if (vehiculos[i].IdVehiculo == id)
+                {
+                    return vehiculos[i];
+                }
+            }
+            return null;
+        }
+
+        //Obtener todos los vehiculos
+        public Vehiculo[] obtenerVehiculos()
+        {
+            return vehiculos;
+        }
+
+        public bool estaLleno()
+        {
+            //Si retorna true, significa que el arreglo está lleno
+            return contador >= vehiculos.Length;
+        }
+       
     }
 }
